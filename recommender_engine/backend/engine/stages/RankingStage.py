a02137cb33750dfcbf7371883268774027fddb17
Ale <alejandro.figueroa.rodriguez.01@gmail.com>
2024-11-20 13:10:11 +0100

    Se agrego el glosario de terminos y los agradecimientos al documento

--- recommender_engine/backend/engine/stages/RankingStage.py
+++ recommender_engine/backend/engine/stages/RankingStage.py
@@ -1,13 +1,21 @@
 from engine.models.LikesModel import LikesModel
 from engine.stages.AbstractStage import AbstractStage
 
-class RankingStage(AbstractStage, ):
+class RankingStage(AbstractStage):
+    _instance = None  # Atributo de clase para almacenar la instancia única
     model: LikesModel
     name: str
 
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(RankingStage, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self) -> None:
-        self.model = LikesModel
-        self.name = "ranking"
+        if not hasattr(self, 'initialized'):  # Verifica si ya ha sido inicializado
+            self.model = LikesModel
+            self.name = "ranking"
+            self.initialized = True  # Marca como inicializado
 
     def __str__(self) -> str:
         return f"Stage({self.name})"
--- recommender_engine/backend/engine/stages/RetrievalStage.py
+++ recommender_engine/backend/engine/stages/RetrievalStage.py
@@ -2,13 +2,20 @@ from engine.models.RetrievalModel import RetrievalModel
 from engine.stages.AbstractStage import AbstractStage
 
 class RetrievalStage(AbstractStage):
+    _instance = None  # Atributo de clase para almacenar la instancia única
     model: RetrievalModel
     name: str
 
-    def __init__(self) -> None:
-        self.model = RetrievalModel
-        self.name = "retrieval"
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(RetrievalStage, cls).__new__(cls)
+        return cls._instance
 
+    def __init__(self) -> None:
+        if not hasattr(self, 'initialized'):  # Verifica si ya ha sido inicializado
+            self.model = RetrievalModel
+            self.name = "retrieval"
+            self.initialized = True  # Marca como inicializado
 
     def __str__(self) -> str:
         return f"Stage({self.name})"
@@ -17,4 +24,4 @@ class RetrievalStage(AbstractStage):
         pass
 
     def outputs(self):
-        pass    
+        pass 
